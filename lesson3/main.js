3a6042da9a0839c0194383571c7d9c5b79f6d05b
MichaelKrasnovskyi <michael.krasnovskyi@gmail.com>
2025-03-25 10:50:13 +0200

    first commit

---
+++ lesson3/main.js
@@ -0,0 +1,62 @@
+// import chalk from 'chalk';
+
+// console.log(chalk.yellowBright('Hello world!'));
+
+// console.log(userEmail);
+
+// let userEmail = 'email@example.ccom';
+
+// let age = 20;
+// const name = 'John';
+
+// console.log(2 + 5);
+
+
+
+// Number
+
+// let userAge = 30;
+// let temperature = -10.5;
+
+// String
+
+// let userName = 'Alex';
+// let carBrand = "BMW";
+// let helloString = `Hello, my name is ${userName}, I am ${userAge} years old`;
+
+// let helloStringOld = 'Hello, my name is ' + userName + ', what about you';
+
+// Boolean
+
+// let isUserLoggedIn = true;
+// let hasNumber = false;
+
+//Undefined
+
+// let undefinedVariable;
+
+//NaN
+
+// let result = 5 / 'hello';
+
+// console.log(10 / 0);
+
+// let number = 12345567891234519392151925915915295215915295219;
+// let numberBig = 12345567891234519392151925915915295215915295219n;
+// console.log(numberBig)
+
+
+// const uniqKey = Symbol('uniq value');
+// const uniqKeyNew = Symbol('uniq value');
+
+
+// let person = {
+//     name: 'Sergo',
+//     age: 5
+// }
+
+// Null
+
+// let noValue = null;
+
+// console.log(typeof noValue);
